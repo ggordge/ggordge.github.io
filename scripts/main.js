350c395fd42a31375599cc3a25181fc75bbfcb93
ggordge <Geraldgordge@gmail.com>
2024-06-10 14:16:23 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/arny.gif") {
+        myImage.setAttribute("src", "images/arny2.gif");
+    }
+    else {
+        myImage.setAttribute("src", "images/arny.gif");
+    }
+}
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Arny is cumming, ${myName}`;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Arny is cumming, ${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+}
