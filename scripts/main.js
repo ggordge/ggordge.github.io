904702202e0d51c813f4af60594d19f66d482a7b
ggordge <Geraldgordge@gmail.com>
2024-06-10 15:54:54 +0100

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,7 @@ function setUserName() {
         setUserName();
     } else {
     localStorage.setItem("name", myName);
-    myHeading.textContent = `Arny is cumming, ${myName}`;
+    myHeading.textContent = `Arnie is cumming, ${myName}`;
     }
 }
 
@@ -27,7 +27,7 @@ if (!localStorage.getItem("name")) {
     setUserName();
 } else {
     const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Arny is cumming, ${storedName}`;
+    myHeading.textContent = `Arnie is cumming, ${storedName}`;
 }
 
 myButton.onclick = () => {
